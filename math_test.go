3c10b810fbfb5d0bc446a4d716613c2f4463228f
JB <jonathan.jb.br@gmail.com>
2024-07-25 03:51:57 +0000

    ajuste

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := subtrai(15, 15)
+	total := soma(15, 15)
 
 	if total != 30 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
@@ -13,7 +13,7 @@ func TestSoma(t *testing.T) {
 
 func TestSubtrai(t *testing.T) {
 
-	total := soma(16, 15)
+	total := subtrai(16, 15)
 
 	if total != 1 {
 		t.Errorf("Resultado da subtração é inválido: Resultado %d. Esperado: %d", total, 1)
