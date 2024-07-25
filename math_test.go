bf518ee9a53bab892a5217e2274b61284e06faee
JB <jonathan.jb.br@gmail.com>
2024-07-25 03:29:10 +0000

    feat: subtrai

--- math_test.go
+++ math_test.go
@@ -10,3 +10,12 @@ func TestSoma(t *testing.T) {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
+
+func TestSubtrai(t *testing.T) {
+
+	total := soma(16, 15)
+
+	if total != 1 {
+		t.Errorf("Resultado da subtração é inválido: Resultado %d. Esperado: %d", total, 1)
+	}
+}
